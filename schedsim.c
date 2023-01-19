ed8f8a1be863c935ad49397ffbc6d1425b7cfb2d
Petrica81 <95485582+Petrica81@users.noreply.github.com>
2023-01-19 19:54:31 +0200

    adaugat biblioteca lipsa + cpu util pentru SJN

--- schedsim.c
+++ schedsim.c
@@ -10,6 +10,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <semaphore.h>
+#include <sys/queue.h>
 
 ///Round-Robin scheduling
 ///preemptive algorithm
@@ -96,52 +97,55 @@ struct proc proc_int(int id, int arrival_time, int burst_time){
     aux.id = id;
     return aux;
 }
-void SJN(int *burst_time, int *arrival_time, int *temp, int *wait_time, int *turnaround_time, int limit) {
-    int *wt_time, *td_time;
-    struct proc *procese;
-    procese = calloc(limit+1, sizeof(struct proc));
-    wt_time = (int*) calloc(limit+1, sizeof(int));
-          td_time = (int*) calloc(limit+1, sizeof(int));
-          wt_time[0] = 0;
-          int clock = arrival_time[0];
-        
-    TAILQ_HEAD(procq, proc);
-        
-    for(int i = 0; i < limit; i++){
-            procese[i] = proc_int(i,arrival_time[i],burst_time[i]);
-    }
-    
-    struct procq q;
-    TAILQ_INIT(&q);
-    int i = 0;
-    while(!TAILQ_EMPTY(&q) || i < limit){
-        while(procese[i].at <= clock && i < limit){
-            struct proc *p;
-            int gasit = 0;
-            TAILQ_FOREACH(p, &q, tailq)
-                if(p->bt > procese[i].bt){
-                    gasit = 1;
-                    TAILQ_INSERT_BEFORE(p, &procese[i], tailq);
-                    break;
-                }
-            if(gasit == 0) TAILQ_INSERT_TAIL(&q, &procese[i], tailq);
-            i++;
-        }
-        struct proc *aux = TAILQ_FIRST(&q);
-        wt_time[aux->id] = clock - aux->at;
-        clock = clock + aux->bt;
-        TAILQ_REMOVE(&q, aux, tailq);
-    }
-     
-    printf("\nProcess ID\tBurst Time\t Turnaround Time\t Waiting Time\n");
-    for(int i = 0; i < limit; i++) {
-        (*wait_time) += wt_time[i];
-        (*turnaround_time) += (burst_time[i] + wt_time[i]);
-        printf("\nProcess[%d]\t%d\t\t %d\t\t\t %d", i + 1, burst_time[i], burst_time[i] + wt_time[i], wt_time[i]);
-    }
-    free(wt_time);
-    free(td_time);
-    free(procese);
+void SJN(int *burst_time, int *arrival_time, int *temp, int *wait_time, int *turnaround_time,int *CPU_util, int limit) {
+	int *wt_time;
+	struct proc *procese;
+	procese = calloc(limit+1, sizeof(struct proc));
+	wt_time = (int*) calloc(limit+1, sizeof(int));
+      	wt_time[0] = 0;
+      	int clock = arrival_time[0];
+	    
+	TAILQ_HEAD(procq, proc);
+	    
+	for(int i = 0; i < limit; i++){
+	    	procese[i] = proc_int(i,arrival_time[i],burst_time[i]);
+	}
+	
+	(*CPU_util) -= clock;
+	struct procq q;
+	TAILQ_INIT(&q);
+	int i = 0;
+	while(!TAILQ_EMPTY(&q) || i < limit){
+		while(procese[i].at <= clock && i < limit){
+			struct proc *p;
+			int gasit = 0;
+			TAILQ_FOREACH(p, &q, tailq)
+				if(p->bt > procese[i].bt){
+					gasit = 1;
+					TAILQ_INSERT_BEFORE(p, &procese[i], tailq);
+					break;
+				}
+			if(gasit == 0) TAILQ_INSERT_TAIL(&q, &procese[i], tailq);
+			i++;
+		}
+		if(!TAILQ_EMPTY(&q)){
+			struct proc *aux = TAILQ_FIRST(&q);
+			wt_time[aux->id] = clock - aux->at;
+			clock = clock + aux->bt;
+			TAILQ_REMOVE(&q, aux, tailq);
+		}
+		else clock += 1;
+	}
+	 
+	printf("\nProcess ID\tBurst Time\t Turnaround Time\t Waiting Time\n");
+	for(int i = 0; i < limit; i++) {
+	    (*wait_time) += wt_time[i];
+	    (*turnaround_time) += (burst_time[i] + wt_time[i]);
+	    printf("\nProcess[%d]\t%d\t\t %d\t\t\t %d", i + 1, burst_time[i], burst_time[i] + wt_time[i], wt_time[i]);
+	}
+	(*CPU_util) += clock;
+	free(wt_time);
+	free(procese);
 }
 
 //shortest remaining time
@@ -199,7 +203,6 @@ void SRT(int *burst_time, int *arrival_time, int *temp, int *wait_time, int *tur
 	    printf("\nProcess[%d]\t%d\t\t %d\t\t\t %d", i + 1, burst_time[i], burst_time[i] + wt_time[i], wt_time[i]);
 	}
 	(*CPU_util) += clock;
-	printf("%ls\n",CPU_util);
 	free(wt_time);
 	free(bt_time);
 	free(procese);
@@ -209,43 +212,46 @@ void SRT(int *burst_time, int *arrival_time, int *temp, int *wait_time, int *tur
 ///din input.txt si priority.txt vom alege datele de intrare
 int main(int argc, char** argv) {    
     int limit, x, alg_option;
-    int wait_time = 0, turnaround_time = 0, *arrival_time, *burst_time, *temp;
-    float average_wait_time, average_turnaround_time;
-    
-    int fisier = open(argv[1], O_RDONLY);
-    char input[1000];
-    read(fisier, input, 1000);
-    close(fisier);
-    char *token = strtok(input," \n");
-    limit = atoi(token);
-    x = limit;
-    
-    arrival_time = (int*) calloc(limit+1, sizeof(int));
-    burst_time = (int*) calloc(limit+1, sizeof(int));
-    temp = (int*) calloc(limit+1, sizeof(int));
-    
-    for(int i = 0 ; i < limit; i++){
-        token = strtok(NULL, " \n");
-        arrival_time[i] = atoi(token);
-        token = strtok(NULL, " \n");
-        burst_time[i] = atoi(token); 
-    }
-    
+	int wait_time = 0, turnaround_time = 0, CPU_util = 0, burst_total = 0, *arrival_time, *burst_time, *temp;
+	float average_wait_time, average_turnaround_time;
+	
+	int fisier = open(argv[1], O_RDONLY);
+	char input[1000];
+	read(fisier, input, 1000);
+	close(fisier);
+	char *token = strtok(input," \n");
+	limit = atoi(token);
+	x = limit;
+	
+	arrival_time = (int*) calloc(limit+1, sizeof(int));
+	burst_time = (int*) calloc(limit+1, sizeof(int));
+	temp = (int*) calloc(limit+1, sizeof(int));
+	
+	for(int i = 0 ; i < limit; i++){
+		token = strtok(NULL, " \n");
+		arrival_time[i] = atoi(token);
+		token = strtok(NULL, " \n");
+		burst_time[i] = atoi(token); 
+		burst_total += burst_time[i];
+	}
+	
     
        printf("Enter the algorithm option: ");
      scanf("%d", &alg_option);
-     if(alg_option == 1) {
+     if(alg_option == 1)
          FCFS(burst_time, arrival_time, temp, &wait_time, &turnaround_time, limit);
-     }
-     if(alg_option == 2) {
+     if(alg_option == 2)
          Round_Robin(burst_time, arrival_time, temp, &wait_time, &turnaround_time, limit, &x);
-    }
-    average_wait_time = wait_time * 1.0 / limit;
+     if(alg_option == 3)
+		SRT(burst_time, arrival_time, temp, &wait_time, &turnaround_time, &CPU_util, limit);
+     if(alg_option == 4)
+		SJN(burst_time, arrival_time, temp, &wait_time, &turnaround_time, &CPU_util, limit);
+    	average_wait_time = wait_time * 1.0 / limit;
        average_turnaround_time = turnaround_time * 1.0 / limit;
        
        printf("\n\nAverage Waiting Time: %f", average_wait_time);
       printf("\nAvg Turnaround Time: %f\n", average_turnaround_time);
-      
+      printf("CPU Utilization: %f%% \n", burst_total * 100.0 / CPU_util);
       free(arrival_time);
       free(burst_time);
       free(temp);
